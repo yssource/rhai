a4cabc1ac71f9a3ff2b1c44a68535738c231ffeb
Stephen Chung <Stephen.Chung@intexact.com>
2020-06-08 14:10:06 +0800

    Better String parameter error message.

--- tests/string.rs
+++ tests/string.rs
@@ -168,7 +168,7 @@ fn test_string_fn() -> Result<(), Box<EvalAltResult>> {
 
     assert!(matches!(
         *engine.eval::<INT>(r#"foo3("hello")"#).expect_err("should error"),
-        EvalAltResult::ErrorFunctionNotFound(ref x, _) if x == "foo3 (string)"
+        EvalAltResult::ErrorFunctionNotFound(err, _) if err == "foo3 (&str | ImmutableString)"
     ));
 
     Ok(())
