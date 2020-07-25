1aab77df1cc924a13f9e6a0c9054a04903550912
John-John Tedro <udoprog@tedro.se>
2020-07-25 08:32:43 +0200

    Avoid scanning over slices to validate numerical literals

--- tests/number_literals.rs
+++ tests/number_literals.rs
@@ -14,6 +14,7 @@ fn test_hex_literal() -> Result<(), Box<EvalAltResult>> {
     let engine = Engine::new();
 
     assert_eq!(engine.eval::<INT>("let x = 0xf; x")?, 15);
+    assert_eq!(engine.eval::<INT>("let x = 0Xf; x")?, 15);
     assert_eq!(engine.eval::<INT>("let x = 0xff; x")?, 255);
 
     Ok(())
@@ -24,6 +25,7 @@ fn test_octal_literal() -> Result<(), Box<EvalAltResult>> {
     let engine = Engine::new();
 
     assert_eq!(engine.eval::<INT>("let x = 0o77; x")?, 63);
+    assert_eq!(engine.eval::<INT>("let x = 0O77; x")?, 63);
     assert_eq!(engine.eval::<INT>("let x = 0o1234; x")?, 668);
 
     Ok(())
@@ -34,6 +36,7 @@ fn test_binary_literal() -> Result<(), Box<EvalAltResult>> {
     let engine = Engine::new();
 
     assert_eq!(engine.eval::<INT>("let x = 0b1111; x")?, 15);
+    assert_eq!(engine.eval::<INT>("let x = 0B1111; x")?, 15);
     assert_eq!(
         engine.eval::<INT>("let x = 0b0011_1100_1010_0101; x")?,
         15525
