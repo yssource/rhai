cebbc2b607dcdbd1de6f83be5d93ddc06c4d68ac
Stephen Chung <schungx@live.com>
2020-12-23 12:11:53 +0800

    Fix duplicate Ident export.

--- src/lib.rs
+++ src/lib.rs
@@ -179,9 +179,7 @@ pub use token::{get_next_token, parse_string_literal, InputStream, Token, Tokeni
 
 #[cfg(feature = "internals")]
 #[deprecated(note = "this type is volatile and may change")]
-pub use ast::{
-    BinaryExpr, CustomExpr, Expr, FnCallExpr, Ident, Ident, ReturnType, ScriptFnDef, Stmt,
-};
+pub use ast::{BinaryExpr, CustomExpr, Expr, FnCallExpr, Ident, ReturnType, ScriptFnDef, Stmt};
 
 #[cfg(feature = "internals")]
 #[deprecated(note = "this type is volatile and may change")]
