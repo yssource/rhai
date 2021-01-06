67a85a19ae581939e0fc7c69c37567910a98b3b7
Stephen Chung <schungx@live.com>
2021-01-06 19:30:58 +0800

    Fix codegen test.

--- codegen/ui_tests/rhai_mod_unknown_type.rs
+++ codegen/ui_tests/rhai_mod_unknown_type.rs
@@ -9,13 +9,16 @@ pub struct Point {
 #[export_module]
 pub mod test_module {
     pub use super::Point;
-    pub fn test_fn(input: Point) -> bool {
+    pub fn test_fn(input: Pointer) -> bool {
         input.x < input.y
     }
 }
 
 fn main() {
-    let n = Point { x: 0.0, y: 10.0 };
+    let n = Point {
+        x: 0.0,
+        y: 10.0
+    };
     if test_module::test_fn(n) {
         println!("yes");
     } else {
